1d24648e0fd4ae2ab46825d9acf6f0779a241698
Soheil <iamsoheil.r@gmail.com>
2024-08-10 15:30:40 +0330

    Add index.html and css files

--- my_site/settings.py
+++ my_site/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STAICFILES_DIRS = [BASE_DIR / "static"]
